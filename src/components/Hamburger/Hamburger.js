56a96ad3d4883b456777c411212a6085d19a9c1d
Omar4589 <omar4589@hotmail.com>
2023-09-09 19:57:06 -0500

    menu and links

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -4,42 +4,48 @@ import "./hamburger.css";
 const Hamburger = () => {
   const { themeState } = useThemeContext();
   return (
-    <div className="outer-menu relative">
+    <div
+      id="hamburger"
+      style={{ color: themeState.menuText }}
+      className="main outer-menu relative"
+    >
       <input
+        id="hamburger-checkbox"
         aria-labelledby="menu"
         className="checkbox-toggle link absolute bottom-4 right-5 w-6 h-6 opacity-0 cursor-pointer"
         type="checkbox"
         aria-label="menu"
       />
       <div
+        id="hamburger-background"
         className={`hamburger absolute bottom-4 right-5 w-6 h-6 flex items-center justify-center `}
       >
         <div
+          id="hamburger-lines"
           style={{ backgroundColor: themeState.text }}
           className="relative w-full items-center"
         ></div>
       </div>
-      <div className="menu absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div
+        id="menu-container"
+        className="menu absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
         <div
+          id="menu-background"
           style={{ backgroundColor: themeState.text }}
-          className="relative w-full h-full flex items-center justify-center opacity-70"
+          className="w-full h-full absolute opacity-80"
+        ></div>
+        <div
+       
+          id="menu-links"
+          className="text-5xl text-left relative top-60 flex flex-col space-y-5 items-center justify-center "
         >
-          <div className="text-white">
-            <ul>
-              <li>
-                <a href="#home">Home</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#services">Services</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
+          <a href="#home">Home</a>
+          <a href="#my-skills">My Skills</a>
+          <a href="#my-projects">My Projects</a>
+          <a href="#about-me">About Me</a>
+          <a href="#contact-me">Contact Me</a>
+          <a href="#resume">Resume</a>
         </div>
       </div>
     </div>
--- src/utils/ThemeContext/themes.js
+++ src/utils/ThemeContext/themes.js
@@ -3,6 +3,7 @@ export const lightTheme = {
   name: "light",
   background: "white",
   text: "black",
+  menuText:"white"
   // ... (any other styles you want to define)
 };
 
@@ -10,5 +11,6 @@ export const darkTheme = {
   name: "dark",
   background: "black",
   text: "white",
+  menuText:"black"
   // ... (any other styles you want to define)
 };
