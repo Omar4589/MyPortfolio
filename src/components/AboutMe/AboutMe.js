14d83d0e57a4c052560ce519cb6d1b77f23ceae1
Omar4589 <omar4589@hotmail.com>
2024-02-14 00:24:23 -0600

    updates about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -21,54 +21,43 @@ const AboutMe = () => {
           ABOUT ME
         </h1>
 
-        <div className="mx-0  lg:mx-4 flex flex-col jusify-center  items-center space-y-2 xl:space-y-2 font-robotoLight leading-7 md:leading-7 lg:leading-9 xl:leading-0">
-          <p className="text-base  md:text-lg  md:mx-2 leading-7 lg:text-lg  xl:text-lg ">
+        <div className="mx-0  lg:mx-4 flex flex-col jusify-center   space-y-2 xl:space-y-2 font-robotoLight ">
+          <p className="text-base  md:text-lg  md:mx-2  lg:text-lg  xl:text-lg leading- md:leading-7  xl:leading-0">
             As a recent graduate stepping into the dynamic world of{" "}
             <span className="font-roboto">web and software development</span>,
             I've quickly learned that this field is not just about coding; it's
             about <span className="font-roboto">constant evolution</span>. My
-            decision to embark on this journey was driven by my fascination with
-            the ever-growing and ever-changing nature of{" "}
-            <span className="font-roboto">technology</span>.
-          </p>
-          <p className="text-base  md:text-lg  md:mx-2 leading-7 lg:text-lg xl:text-lg">
-            My love for acquiring new knowledge and my eagerness to be
-            challenged are what fuel my passion for development. With a mindset
-            geared towards <span className="font-roboto">problem-solving</span>{" "}
-            and <span className="font-roboto">critical thinking</span>, I
-            approach every project with the determination to find{" "}
+            decision to embark on this journey was driven by my love for
+            acquiring new knowledge and my eagerness to be challenged. With a
+            mindset geared towards{" "}
+            <span className="font-roboto">problem-solving</span> and{" "}
+            <span className="font-roboto">critical thinking</span>, I approach
+            every project with the determination to find{" "}
             <span className="font-roboto">innovative solutions</span>.
           </p>
 
-          <p className="text-base  md:text-lg  md:mx-2 leading-7 lg:text-lg xl:text-lg">
+          <p className="text-base  md:text-lg  md:mx-2  lg:text-lg xl:text-lg leading- md:leading-7  xl:leading-0">
             My work ethic is built on a foundation of{" "}
             <span className="font-roboto">self-motivation</span>,{" "}
             <span className="font-roboto">determination</span>, and{" "}
             <span className="font-roboto">flexibility</span>. I strive to be
             better every day, not just in my technical abilities but also in my
             capacity to{" "}
-            <span className="font-roboto">collaborate and adapt</span>. If you're seeking someone who
-            embodies the spirit of{" "}
+            <span className="font-roboto">collaborate and adapt</span>. If
+            you're seeking someone who embodies the spirit of{" "}
             <span className="font-roboto">perseverance</span>,{" "}
             <span className="font-roboto">innovation</span>, and{" "}
             <span className="font-roboto">continuous improvement</span>, you've
-            found the right person.
-          </p>
-          {/* <p className="text-base  md:text-lg  md:mx-2 leading-7 lg:text-lg xl:text-lg">
-            In addition to my technical skills and work ethic, I bring to the
-            table a unique blend of{" "}
-            <span className="font-roboto">creativity</span> and{" "}
-            <span className="font-roboto">analytical thinking</span>. My
-            background in photography and my diverse interests outside of coding
-            inform my approach to development, allowing me to tackle projects
-            with a <span className="font-roboto">unique perspective</span>.
-          </p> */}
-          <p className="text-base  md:text-lg  md:mx-2 leading-7 lg:text-lg xl:text-lg">
-            I am excited to continue{" "}
+            found the right person. I am excited to continue{" "}
             <span className="font-roboto">growing</span>,{" "}
             <span className="font-roboto">learning</span>, and{" "}
             <span className="font-roboto">contributing</span> to the development
-            community. <span className="font-roboto">Let's build something great together!</span>
+            community.{" "}
+          </p>
+          <p className="text-base  md:text-lg  md:mx-2  lg:text-lg xl:text-lg leading- md:leading-7  xl:leading-0">
+            <span className="font-roboto">
+              Let's build something great together!
+            </span>
           </p>
         </div>
       </div>
